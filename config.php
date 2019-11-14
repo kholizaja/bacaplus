2c11b247f5843792a820caea42db2d667f6fd64a
kholizaja <56310771+kholizaja@users.noreply.github.com>
2019-11-14 17:18:40 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$uid="Facebook_460526164536282";
+$raw_uid="55aa8f4e-970f-420a-9a84-5d06f461738c";
+$user_name="Kholizwiyono";
+$invite_code="F26lovOBUQ";
+$user_token="Abzvx33f73JO0Yeg";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="c0e59e7393a32a4e";
+$device_type="Xiaomi++Redmi+Note+5A";
+
+?>
